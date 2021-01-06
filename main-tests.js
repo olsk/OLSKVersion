fa8c9399a7cad7ef21c44391d258dab367df6d7a
Rosano Coutinho <pub@xeari.com>
2021-01-06 12:35:41 -0300

    Spec OLSKVersionAdd

--- main-tests.js
+++ main-tests.js
@@ -1,1 +1,41 @@
+const { throws, deepEqual } = require('assert');
 
+const mod = require('./main.js');
+
+describe('OLSKVersionAdd', function test_OLSKVersionAdd() {
+
+	it('throws if param1 not object', function () {
+		throws(function () {
+			mod.OLSKVersionAdd(null, Math.random().toString(), Math.random());
+		}, /OLSKErrorInputNotValid/);
+	});
+	
+	it('throws if param2 not string', function () {
+		throws(function () {
+			mod.OLSKVersionAdd({}, null, Math.random());
+		}, /OLSKErrorInputNotValid/);
+	});
+	
+	it('returns param1', function () {
+		const item = {
+			[Math.random().toString()]: Math.random().toString(),
+		};
+
+		deepEqual(mod.OLSKVersionAdd(item, Math.random().toString(), Math.random()), item);
+	});
+	
+	it('creates array at param2 if not exists', function () {
+		const item = Math.random().toString();
+
+		deepEqual(mod.OLSKVersionAdd({}, item, item)[item], [item]);
+	});
+	
+	it('adds param3 to array at param2 if exists', function () {
+		const item = Math.random().toString();
+
+		deepEqual(mod.OLSKVersionAdd({
+			[item]: [item]
+		}, item, item)[item], [item, item]);
+	});
+	
+});
--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const mod = {
 
+	OLSKVersionAdd (param1, param2, param3) {
+		if (typeof param1 !== 'object' || param1 === null) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		if (typeof param2 !== 'string') {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return Object.assign(param1, {
+			[param2]: (param1[param2] || []).concat(param3),
+		});
+	},	
 
 };
 
